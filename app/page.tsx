2ae1b930eea90e04dbd7e39b74796bdfed3e1527
Tharmithan Nanthakumar <kumartharmi98@gmail.com>
2025-11-06 11:01:52 +0530

    state component removed

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <WhyUsSection />
       <OurProcessSection />
       <LatestWorkSection />
-      <StatsSection />
+      {/* <StatsSection /> */}
       <TestimonialsSection />
       <PricingSection />
       <LatestNewsSection />
